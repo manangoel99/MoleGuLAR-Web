7f9b85dbf0f46e8de5917f44190ca2bc06cfaf23
Manan Goel <manan.goel@research.iiit.ac.in>
2022-05-03 13:28:24 +0530

    Added logic for dockerization

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import enum
+import os
 
 from databases import Database
 from sqlalchemy import (ARRAY, JSON, Column, Enum, ForeignKey, Integer,
@@ -12,7 +13,7 @@ class JobStatus(enum.Enum):
     failed = 3
 
 
-DATABASE_URL = 'postgresql://devuser:dev_password@localhost/movie_db'
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
